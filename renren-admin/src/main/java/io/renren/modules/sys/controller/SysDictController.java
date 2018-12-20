b49dee456480dfeb46b9aff8b2e5b404f2e7fb1f
tom_glb <tom_gla123@163.com>
2018-12-20 23:29:20 +0800

    提交修改字典的bug

--- renren-admin/src/main/java/io/renren/modules/sys/controller/SysDictController.java
+++ renren-admin/src/main/java/io/renren/modules/sys/controller/SysDictController.java
@@ -2,15 +2,19 @@ package io.renren.modules.sys.controller;
 
 import io.renren.common.utils.PageUtils;
 import io.renren.common.utils.R;
+import io.renren.common.validator.ValidatorUtils;
+import io.renren.modules.sys.entity.SysDictEntity;
 import io.renren.modules.sys.service.SysDictService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.Map;
 
 /**
@@ -19,8 +23,9 @@ import java.util.Map;
  * @author wudepeng
  * @since 2018/12/19
  */
-//@Api(value = "字典控制类")
+@Api(value = "字典控制类")
 @RestController
+@RequestMapping("/sys/dict")
 public class SysDictController {
 
 
@@ -28,12 +33,37 @@ public class SysDictController {
     private SysDictService dictService;
 
 
-//    @ApiOperation(value = "列表查询")
-
+    @ApiOperation(value = "列表查询")
     @RequestMapping("/list")
-    @RequiresPermissions("sys:user:list")
+    @RequiresPermissions("sys:dict:list")
     public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = dictService.queryPage(params);
         return R.ok().put("page", page);
+    }
+
+    @ApiOperation(value = "新增")
+    @RequestMapping("/save")
+    @RequiresPermissions("sys:dict:save")
+    public R save(@RequestBody SysDictEntity entity){
+        ValidatorUtils.validateEntity(entity);
+        dictService.insert(entity);
+        return R.ok();
+    }
+
+    @ApiOperation(value = "修改")
+    @RequestMapping("/update")
+    @RequiresPermissions("sys:dict:update")
+    public R update(@RequestBody SysDictEntity entity){
+        ValidatorUtils.validateEntity(entity);
+        dictService.updateAllColumnById(entity);
+        return R.ok();
+    }
+
+    @ApiOperation(value = "删除")
+    @RequestMapping("/delete")
+    @RequiresPermissions("sys:dict:delete")
+    public R deleteByDictId(@RequestBody Long[] ids){
+        dictService.deleteBatchIds(Arrays.asList(ids));
+        return R.ok();
     }
 }

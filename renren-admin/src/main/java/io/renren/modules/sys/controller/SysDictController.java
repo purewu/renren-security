e5351eb74c770d29dd71ba966a0bc6a522e34a80
tom_glb <tom_gla123@163.com>
2018-12-21 20:53:18 +0800

    完成字典模块

--- renren-admin/src/main/java/io/renren/modules/sys/controller/SysDictController.java
+++ renren-admin/src/main/java/io/renren/modules/sys/controller/SysDictController.java
@@ -4,17 +4,16 @@ import io.renren.common.utils.PageUtils;
 import io.renren.common.utils.R;
 import io.renren.common.validator.ValidatorUtils;
 import io.renren.modules.sys.entity.SysDictEntity;
+import io.renren.modules.sys.entity.SysRoleEntity;
 import io.renren.modules.sys.service.SysDictService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -50,12 +49,22 @@ public class SysDictController {
         return R.ok();
     }
 
+    @ApiOperation(value = "字典信息")
+    @RequestMapping("/info/{id}")
+    @RequiresPermissions("sys:dict:info")
+    public R info(@PathVariable("id") Long id){
+        SysDictEntity dict = dictService.selectById(id);
+
+        return R.ok().put("dict", dict);
+    }
+
     @ApiOperation(value = "修改")
     @RequestMapping("/update")
     @RequiresPermissions("sys:dict:update")
     public R update(@RequestBody SysDictEntity entity){
         ValidatorUtils.validateEntity(entity);
         dictService.updateAllColumnById(entity);
+
         return R.ok();
     }
 
